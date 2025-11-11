31bf80a53218edfdcf599e745091b0c39e9d54f8
mic-cyberkid <93459729+mic-cyberkid@users.noreply.github.com>
2025-11-11 15:24:57 +0000

    Create download.py

---
+++ download.py
@@ -0,0 +1,37 @@
+import requests
+import re
+
+def download(file_id, destination):
+    session = requests.Session()
+    base_url = "https://drive.usercontent.google.com/download"
+
+    params = {"id": file_id, "export": "download"}
+    response = session.get(base_url, params=params, stream=True)
+
+    if "text/html" in response.headers.get("Content-Type", ""):
+        # We must extract the confirm token from the HTML
+        html = response.text
+        token_match = re.search(r'name="confirm" value="([^"]+)"', html)
+
+        if not token_match:
+            raise RuntimeError("Could not find confirm token in response HTML!")
+
+        token = token_match.group(1)
+        params["confirm"] = token
+        print("Downloading file....")
+        response = session.get(base_url, params=params, stream=True)
+
+    save_file(response, destination)
+
+
+def save_file(response, filename):
+    with open(filename, "wb") as f:
+        for chunk in response.iter_content(32768):
+            if chunk:
+                f.write(chunk)
+
+
+if __name__ == "__main__":
+    download("1SmfR3QEimj25jngQSxcQtARtrZ7Aux6D",
+             "PhysicsChatbotServer-Windows.zip")
+    print("✅ Download complete")
